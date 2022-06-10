738be34840aea6a6d21433eaa5d1d8242c05c5a5
Hiền Hiềnn <95482961+hienhienn@users.noreply.github.com>
2022-06-10 20:42:31 +0700

    test
    2 ý đầu tiên

--- src/main/java/myhustwork/luonvuituoi/Model/Calculator.java
+++ src/main/java/myhustwork/luonvuituoi/Model/Calculator.java
@@ -10,4 +10,21 @@ package myhustwork.luonvuituoi.Model;
  */
 public class Calculator {
     System.out.println("Hello");
+    public static Boolean WarningBalance(double balance) { //canh bao
+        return balance < 0;
+    }
+    public static void PercentCategories(int Month) {
+        double sum;
+        double[] sumCategories = new double[12];
+        double[] percentCategories = new double[12];
+        int i;
+        if(Transaction.Date.getMonth == Month){
+            sum += Transaction.Fluctuation.getAmount;
+            i = Transaction.Categories.getResult;
+            sumCategories[i] += Transaction.Fluctuation.getAmount;
+        }
+        for(int i = o; i < 12; i++) {
+            percentCategories[i] = sumCategories[i]/sum;
+        }
+    }
 }
