7dad3587aaddb43efc4ccaa4fa2a2fb969935f9f
HaAnhVu2306 <anhvuha2306@gmail.com>
2022-07-12 00:03:09 +0700

    Update 12/7

--- src/main/java/myhustwork/luonvuituoi/BLL/FluctuationBLL.java
+++ src/main/java/myhustwork/luonvuituoi/BLL/FluctuationBLL.java
@@ -5,28 +5,31 @@ import java.util.Calendar;
 import myhustwork.luonvuituoi.DAO.FluctuationDAO;
 import myhustwork.luonvuituoi.DTO.AccountDTO;
 import myhustwork.luonvuituoi.DTO.FluctuationDTO;
-        
-public class FluctuationBLL extends Money {//bien dong so du//
+import myhustwork.luonvuituoi.DTO.MoneyDTO;
+
+public class FluctuationBLL extends MoneyDTO {//bien dong so du//
+
     /**
      * Warning if there's a chance balance < 0
-     * @return 
+     *
+     * @return
      */
     public static Boolean WarningBalance() { //canh bao
         double save_per_month = AccountDTO.getSave_per_month(); // account lấy từ database
         double balance = AccountDTO.getBalance();
         return balance - save_per_month < 0;
     }
-    
+
     public static double[] PercentCategories(Date date1, Date date2) {
         double[] sumCategories = new double[26]; // có 25 hạng mục 
         double[] percentCategories = new double[26];
-        for(int j = 1; j <= 25; j++) {
+        for (int j = 1; j <= 25; j++) {
             sumCategories[j] = 0;
             percentCategories[j] = -1; // -1 là không tồn tại
         }
         FluctuationDTO[] flucArr = FluctuationDAO.getAllFluctuations();
-        for(FluctuationDTO i: flucArr) {
-            if(i.getDate().after(date1) && i.getDate().before(date2) ){
+        for (FluctuationDTO i : flucArr) {
+            if (i.getDate().after(date1) && i.getDate().before(date2)) {
                 switch (i.getCategory().getCategoryId()) {
                     case 3, 4, 5, 6, 7, 8, 9, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 -> {
                         int j = i.getCategory().getCategoryId();
@@ -34,22 +37,23 @@ public class FluctuationBLL extends Money {//bien dong so du//
                         break;
                     }
                 }
-        for(int j = 3; j <=9; j++) {
-            sumCategories[1] += sumCategories[j]; 
-        }
-        sumCategories[10] = sumCategories[14] + sumCategories[15] + sumCategories[16] + sumCategories[17] + sumCategories[18];
-        sumCategories[11] = sumCategories[19] + sumCategories[20] + sumCategories[21] + sumCategories[22] + sumCategories[23];
-        sumCategories[13] = sumCategories[24] + sumCategories[25];
-        sumCategories[2] = sumCategories[10] + sumCategories[11] + sumCategories[12] + sumCategories[13];
-        
-        for(int j = 3; j <= 9; j++) {
-            percentCategories[j] = sumCategories[j]/sumCategories[1];
-        }
-        for(int j = 10; j <= 25; j++) {
-            percentCategories[j] = sumCategories[j]/sumCategories[2];
+                for (int j = 3; j <= 9; j++) {
+                    sumCategories[1] += sumCategories[j];
+                }
+                sumCategories[10] = sumCategories[14] + sumCategories[15] + sumCategories[16] + sumCategories[17] + sumCategories[18];
+                sumCategories[11] = sumCategories[19] + sumCategories[20] + sumCategories[21] + sumCategories[22] + sumCategories[23];
+                sumCategories[13] = sumCategories[24] + sumCategories[25];
+                sumCategories[2] = sumCategories[10] + sumCategories[11] + sumCategories[12] + sumCategories[13];
+
+                for (int j = 3; j <= 9; j++) {
+                    percentCategories[j] = sumCategories[j] / sumCategories[1];
+                }
+                for (int j = 10; j <= 25; j++) {
+                    percentCategories[j] = sumCategories[j] / sumCategories[2];
+                }
+            }
         }
         return percentCategories;
-        
 //                if(i.getCategory().isIncome()) {
 //                    sumIncome += i.getAmount();
 //                    j = i.getCategory().getCategoryId();
@@ -65,54 +69,50 @@ public class FluctuationBLL extends Money {//bien dong so du//
 //                percentCategoriesIncome[j] = sumCategoriesIncome[j]/sumIncome;
 //                percentCategoriesSpending[j] = sumCategoriesSpending[j]/sumSpending;
 //            }
-    } 
+    }
+
     /**
      * sum per month
      *
-     */  
+     */
     public static void SumPerMonth(int Year) {
         double[] sumIncome = new double[13]; // tổng thu của 12 tháng
         double[] sumSpending = new double[13]; // tổng chi của 12 tháng
-        for(int j = 1; j <= 12; j ++){
+        for (int j = 1; j <= 12; j++) {
             sumIncome[j] = 0;
             sumSpending[j] = 0;
         }
         FluctuationDTO[] flucArr = FluctuationDAO.getAllFluctuations();
-        for(FluctuationDTO i: flucArr) {
+        for (FluctuationDTO i : flucArr) {
             Calendar cal = Calendar.getInstance();
             cal.setTime(i.getDate()); // chuyển Date thành Calendar
-            if(Year == cal.get(Calendar.YEAR) ) {
+            if (Year == cal.get(Calendar.YEAR)) {
                 int j = cal.get(Calendar.MONTH);
-                if(i.getCategory().isIncome()) sumIncome[j] += i.getAmount(); // tính tổng thu từng tháng
-                else sumSpending[j] += i.getAmount(); // tổng chi từng tháng
+                if (i.getCategory().isIncome()) {
+                    sumIncome[j] += i.getAmount(); // tính tổng thu từng tháng
+                } else {
+                    sumSpending[j] += i.getAmount(); // tổng chi từng tháng
+                }
             }
         }
     }
-    
+
     public static double AutoCal() {
         Calendar cal = Calendar.getInstance();
         double balance = AccountDTO.getBalance();
-        if(cal.get(Calendar.DAY_OF_MONTH) == 1){ // sang thang moi
+        if (cal.get(Calendar.DAY_OF_MONTH) == 1) { // sang thang moi
             FluctuationDTO[] flucArr = FluctuationDAO.getAllFluctuations();
-            for(FluctuationDTO i: flucArr) {
-                if(i.isFixed()) {
-                    if(i.getCategory().isIncome()) balance += i.getAmount();
-                    else balance -= i.getAmount();
+            for (FluctuationDTO i : flucArr) {
+                if (i.isFixed()) {
+                    if (i.getCategory().isIncome()) {
+                        balance += i.getAmount();
+                    } else {
+                        balance -= i.getAmount();
+                    }
                 }
             }
-        } 
+        }
         return balance;
     }
-    
-    public static int[] SuggestionNextMonth(Date date1, Date date2) {
-        double[] percentCategories = new double[26];
-        percentCategories = PercentCategories(date1, date2);
-        int[] mark = {0, 0, 0, 0};
-        if(percentCategories[10] > 0.6) mark[0] = 1; // = 1 là tiêu quá
-        if(percentCategories[21] > 0.15) mark[1] = 1;
-        if(percentCategories[19] + percentCategories[22] + percentCategories[23] > 0.15) mark[2] = 1;
-        if(1 - percentCategories[10] - percentCategories[21] - percentCategories[19] - percentCategories[22] - percentCategories[23] > 0.1) mark[3] = 1;
-        return mark;
-    }
-}
 
+}
--- src/main/java/myhustwork/luonvuituoi/BLL/StuffBLL.java
+++ src/main/java/myhustwork/luonvuituoi/BLL/StuffBLL.java
@@ -1,10 +1,11 @@
-
 package myhustwork.luonvuituoi.BLL;
+
 import java.util.Date;
 import java.util.Calendar;
 
 import java.util.Date;
 import java.util.*;
+import static myhustwork.luonvuituoi.BLL.FluctuationBLL.PercentCategories;
 import myhustwork.luonvuituoi.DAO.FluctuationDAO;
 import myhustwork.luonvuituoi.DAO.StuffDAO;
 import myhustwork.luonvuituoi.DTO.AccountDTO;
@@ -12,94 +13,115 @@ import myhustwork.luonvuituoi.DTO.CategoryDTO;
 import myhustwork.luonvuituoi.DTO.FluctuationDTO;
 import myhustwork.luonvuituoi.DTO.StuffDTO;
 
-
 /**
- * 
+ *
  * @author vvlalalove193
  */
 public class StuffBLL {
-    
-    public StuffDTO[] getAllStuffs(){
+
+    public StuffDTO[] getAllStuffs() {
         return StuffDAO.getAllStuffs();
     }
-    
-    public void Suggestion(Date date1,Date date2){
-        double ThuThanghientai = 0,Chithanghientai = 0,Sodutrongthang = 0;
-        
+
+    public void Suggestion(Date date1, Date date2) {
+        double ThuThanghientai = 0, Chithanghientai = 0, Sodutrongthang = 0;
+
         Calendar cal = Calendar.getInstance();
         Date date = cal.getTime();
-        FluctuationDTO[] flucArr = FluctuationDAO.getAllFluctuations();        
-        for(FluctuationDTO i: flucArr){ //duyet tat ca cac fluctuation
-            if (i.getDate().after(date1) && i.getDate().before(date2) ) {
-                if(i.getCategory().getCategoryType() == CategoryDTO.THU) {
+        FluctuationDTO[] flucArr = FluctuationDAO.getAllFluctuations();
+        for (FluctuationDTO i : flucArr) { //duyet tat ca cac fluctuation
+            if (i.getDate().after(date1) && i.getDate().before(date2)) {
+                if (i.getCategory().getCategoryType() == CategoryDTO.THU) {
                     ThuThanghientai += i.getAmount();
                 } else {
                     Chithanghientai += i.getAmount();
                 }
             }
-        }   
+        }
         Sodutrongthang = ThuThanghientai - Chithanghientai;
-        FluctuationBLL.PercentCategories(date1,date2);
+        FluctuationBLL.PercentCategories(date1, date2);
     }
-    
-    public void Swap(StuffDTO s1,StuffDTO s2){ //Dao vi tri 
-        StuffDTO tmp = new StuffDTO(s1.getCategory(), s1.getNote(), s1.getAmount());
-        s1.setAmount(s2.getAmount()); s1.setCategory(s2.getCategory()); s1.setNote(s2.getNote());
-        s2.setAmount(tmp.getAmount()); s2.setCategory(tmp.getCategory()); s2.setNote(tmp.getNote());
+
+    public void Swap(StuffDTO s1, StuffDTO s2) { //Dao vi tri 
+        StuffDTO tmp = new StuffDTO(s1.getID(),s1.getCategory(), s1.getNote(), s1.getAmount());
+        s1.setAmount(s2.getAmount());
+        s1.setCategory(s2.getCategory());
+        s1.setNote(s2.getNote());
+        s2.setAmount(tmp.getAmount());
+        s2.setCategory(tmp.getCategory());
+        s2.setNote(tmp.getNote());
     }
-    
-    public void StuffSuggestion(StuffDTO[] stuff){
+
+    public void StuffSuggestion(StuffDTO[] stuff) {
         int n = stuff.length;
 //        StuffDTO[] stuff = new StuffDTO[n]; // n: so stuff can mua
         //Lay ra tu database
         Arrays.sort(stuff);
-        for(StuffDTO i: stuff){
+        for (StuffDTO i : stuff) {
             /*
             20:Suc khoe
             21:Cong viec hoc tap
             22:Giai tri
             25:Tra no
-            */
-            if(i.getCategory().getCategoryId()== 25 && i.getAmount()< AccountDTO.getBalance() && stuff[0].getCategory().getCategoryId()!=25){ 
-            //Tra no 
-                Swap(i,stuff[0]);
-                if(stuff[2].getCategory().getCategoryId()==22 || stuff[2].getCategory().getCategoryId()==21){
-                    Arrays.sort(stuff,3,n-1);
-                }
-                else if(stuff[1].getCategory().getCategoryId()==20){
-                    Arrays.sort(stuff,2,n-1);
-                }
-                else{
-                    Arrays.sort(stuff,1,n-1);
+             */
+            if (i.getCategory().getCategoryId() == 25 && i.getAmount() < AccountDTO.getBalance() && stuff[0].getCategory().getCategoryId() != 25) {
+                //Tra no 
+                Swap(i, stuff[0]);
+                if (stuff[2].getCategory().getCategoryId() == 22 || stuff[2].getCategory().getCategoryId() == 21) {
+                    Arrays.sort(stuff, 3, n - 1);
+                } else if (stuff[1].getCategory().getCategoryId() == 20) {
+                    Arrays.sort(stuff, 2, n - 1);
+                } else {
+                    Arrays.sort(stuff, 1, n - 1);
                 }
             }
-            if(i.getCategory().getCategoryId()== 20 && i.getAmount()< AccountDTO.getBalance()){
-            //Suc khoe 
-                    Swap(i,stuff[1]);
-                    Arrays.sort(stuff,2,n-1);   
+            if (i.getCategory().getCategoryId() == 20 && i.getAmount() < AccountDTO.getBalance()) {
+                //Suc khoe 
+                Swap(i, stuff[1]);
+                Arrays.sort(stuff, 2, n - 1);
             }
-            if(i.getCategory().getCategoryId()== 22 & i.getAmount()< AccountDTO.getBalance()){
-            // Giai tri
-               if(stuff[0].getCategory().getCategoryId()==25 && stuff[1].getCategory().getCategoryId()==20){
-                   Swap(i,stuff[2]);
-                   Arrays.sort(stuff,3,n-1);
-               }
-               else if(stuff[0].getCategory().getCategoryId()!=25 && stuff[1].getCategory().getCategoryId()==20){
-                   Swap(i,stuff[0]);
-                   Arrays.sort(stuff,2,n-1); 
-               }
-               else {
-                   Swap(i,stuff[1]);
-                   Arrays.sort(stuff,2,n-1);
-               }
+            if (i.getCategory().getCategoryId() == 22 & i.getAmount() < AccountDTO.getBalance()) {
+                // Giai tri
+                if (stuff[0].getCategory().getCategoryId() == 25 && stuff[1].getCategory().getCategoryId() == 20) {
+                    Swap(i, stuff[2]);
+                    Arrays.sort(stuff, 3, n - 1);
+                } else if (stuff[0].getCategory().getCategoryId() != 25 && stuff[1].getCategory().getCategoryId() == 20) {
+                    Swap(i, stuff[0]);
+                    Arrays.sort(stuff, 2, n - 1);
+                } else {
+                    Swap(i, stuff[1]);
+                    Arrays.sort(stuff, 2, n - 1);
+                }
             }
-            if(i.getCategory().getCategoryId()==21 && i.getAmount()< 4*stuff[2].getAmount()){
+            if (i.getCategory().getCategoryId() == 21 && i.getAmount() < 4 * stuff[2].getAmount()) {
                 //Cong viec hoc tap
-                Swap(i,stuff[2]);
-            }   
+                Swap(i, stuff[2]);
+            }
         }
-        for(StuffDTO i: stuff){
-            if(i.getCategory().getCategoryId()==24) Swap(i,stuff[n-1]);
+        for (StuffDTO i : stuff) {
+            if (i.getCategory().getCategoryId() == 24) {
+                Swap(i, stuff[n - 1]);
+            }
         }
+    }
+
+    public static int[] SuggestionNextMonth(Date date1, Date date2) {
+        double[] percentCategories = new double[26];
+        percentCategories = PercentCategories(date1, date2);
+        int[] mark = {0, 0, 0, 0};
+        if (percentCategories[10] > 0.6) {
+            mark[0] = 1; // = 1 là tiêu quá
+        }
+        if (percentCategories[21] > 0.15) {
+            mark[1] = 1;
+        }
+        if (percentCategories[19] + percentCategories[22] + percentCategories[23] > 0.15) {
+            mark[2] = 1;
+        }
+        if (1 - percentCategories[10] - percentCategories[21] - percentCategories[19] - percentCategories[22] - percentCategories[23] > 0.1) {
+            mark[3] = 1;
+        }
+        return mark;
+        //in ra màn hình: bạn đã tiêu quá (những cái mark[] = 1) , bạn hãy tiêu thêm vào (những cái mark[] = 0)
     }
 }
